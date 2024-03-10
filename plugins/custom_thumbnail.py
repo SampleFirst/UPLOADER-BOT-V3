aabd9ca97b83cbc8769df0b5e3d0eb11e662e912
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-10 23:49:18 +0530

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -27,31 +27,31 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 @Client.on_message(filters.private & filters.photo)
-async def photo_handler(bot: Client, event: Message):
-    if not event.from_user:
-        return await event.reply_text("I don't know about you, sir :(")
-    await add_user_to_database(bot, event)
-    if Config.UPDATES_CHANNEL:
-        fsub = await handle_force_subscribe(bot, event)
+async def photo_handler(client: Client, message: Message):
+    if not message.from_user:
+        return await message.reply_text("I don't know about you, sir :(")
+    await add_user_to_database(client, message)
+    if Config.messageS_CHANNEL:
+        fsub = await handle_force_subscribe(client, message)
         if fsub == 400:
             return
-    editable = await event.reply_text("**👀 Processing...**")
-    await db.set_thumbnail(event.from_user.id, thumbnail=event.photo.file_id)
+    editable = await message.reply_text("**👀 Processing...**")
+    await db.set_thumbnail(message.from_user.id, thumbnail=message.photo.file_id)
     await editable.edit("**✅ Custom thumbnail saved successfully!**")
 
 
 @Client.on_message(filters.private & filters.command(["delthumb", "deletethumbnail"]))
-async def delete_thumb_handler(bot: Client, event: Message):
-    if not event.from_user:
-        return await event.reply_text("I don't know about you, sir :(")
-    await add_user_to_database(bot, event)
-    if Config.UPDATES_CHANNEL:
-        fsub = await handle_force_subscribe(bot, event)
+async def delete_thumb_handler(client: Client, message: Message):
+    if not message.from_user:
+        return await message.reply_text("I don't know about you, sir :(")
+    await add_user_to_database(client, message)
+    if Config.messageS_CHANNEL:
+        fsub = await handle_force_subscribe(client, message)
         if fsub == 400:
             return
 
-    await db.set_thumbnail(event.from_user.id, thumbnail=None)
-    await event.reply_text(
+    await db.set_thumbnail(message.from_user.id, thumbnail=None)
+    await message.reply_text(
         "**🗑️ Custom thumbnail deleted successfully!**",
         reply_markup=InlineKeyboardMarkup(
             [
@@ -64,18 +64,18 @@ async def delete_thumb_handler(bot: Client, event: Message):
 
 
 @Client.on_message(filters.private & filters.command("showthumb"))
-async def viewthumbnail(bot, update):
-    if not update.from_user:
-        return await update.reply_text("I don't know about you, sir :(")
-    await add_user_to_database(bot, update)
+async def viewthumbnail(client, message):
+    if not message.from_user:
+        return await message.reply_text("I don't know about you, sir :(")
+    await add_user_to_database(client, message)
     if Config.UPDATES_CHANNEL:
-        fsub = await handle_force_subscribe(bot, update)
+        fsub = await handle_force_subscribe(client, message)
         if fsub == 400:
             return
-    thumbnail = await db.get_thumbnail(update.from_user.id)
+    thumbnail = await db.get_thumbnail(message.from_user.id)
     if thumbnail is not None:
-        await bot.send_photo(
-            chat_id=update.chat.id,
+        await client.send_photo(
+            chat_id=message.chat.id,
             photo=thumbnail,
             caption="Your current saved thumbnail",
             reply_markup=InlineKeyboardMarkup(
@@ -85,16 +85,16 @@ async def viewthumbnail(bot, update):
                     ]
                 ]
             ),
-            reply_to_message_id=update.message_id)
+            reply_to_message_id=message.message_id)
     else:
-        await update.reply_text(text="No thumbnail found 🤒")
+        await message.reply_text(text="No thumbnail found 🤒")
 
 
-async def get_thumbnail(bot, update, duration, download_directory):
-    thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
-    db_thumbnail = await db.get_thumbnail(update.from_user.id)
+async def get_thumbnail(client, message, duration, download_directory):
+    thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(message.from_user.id) + ".jpg"
+    db_thumbnail = await db.get_thumbnail(message.from_user.id)
     if db_thumbnail is not None:
-        thumbnail = await bot.download_media(message=db_thumbnail, file_name=thumb_image_path)
+        thumbnail = await client.download_media(message=db_thumbnail, file_name=thumb_image_path)
         Image.open(thumbnail).convert("RGB").save(thumbnail)
         img = Image.open(thumbnail)
         img.resize((100, 100))
@@ -105,11 +105,11 @@ async def get_thumbnail(bot, update, duration, download_directory):
     return thumbnail
 
 
-async def get_thumbnail_v2(bot, update, duration, download_directory):
-    thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
-    db_thumbnail = await db.get_thumbnail(update.from_user.id)
+async def get_thumbnail_v2(client, message, duration, download_directory):
+    thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(message.from_user.id) + ".jpg"
+    db_thumbnail = await db.get_thumbnail(message.from_user.id)
     if db_thumbnail is not None:
-        thumbnail = await bot.download_media(message=db_thumbnail, file_name=thumb_image_path)
+        thumbnail = await client.download_media(message=db_thumbnail, file_name=thumb_image_path)
     else:
         thumbnail = await take_screen_shot(download_directory, os.path.dirname(download_directory), random.randint(0, duration - 1))
 
