d676cc19aefe76ade5b9ddda61b4c4407e660bed
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-10 23:50:13 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -18,36 +18,36 @@ logger = logging.getLogger(__name__)
 
 
 @Client.on_callback_query()
-async def button(bot, update):
-    if update.data == "home":
-        await update.message.edit_text(
-            text=Translation.START_TEXT.format(update.from_user.mention),
+async def button(client, query):
+    if query.data == "home":
+        await query.message.edit_text(
+            text=Translation.START_TEXT.format(query.from_user.mention),
             reply_markup=Translation.START_BUTTONS,
             disable_web_page_preview=True
         )
-    elif update.data == "help":
-        await update.message.edit_text(
+    elif query.data == "help":
+        await query.message.edit_text(
             text=Translation.HELP_TEXT,
             reply_markup=Translation.HELP_BUTTONS,
             disable_web_page_preview=True
         )
-    elif update.data == "about":
-        await update.message.edit_text(
+    elif query.data == "about":
+        await query.message.edit_text(
             text=Translation.ABOUT_TEXT,
             reply_markup=Translation.ABOUT_BUTTONS,
             disable_web_page_preview=True
         )
-    elif update.data == "OpenSettings":
-        await update.answer()
-        await OpenSettings(update.message)
-    elif update.data == "showThumbnail":
-        thumbnail = await db.get_thumbnail(update.from_user.id)
+    elif query.data == "OpenSettings":
+        await query.answer()
+        await OpenSettings(query.message)
+    elif query.data == "showThumbnail":
+        thumbnail = await db.get_thumbnail(query.from_user.id)
         if not thumbnail:
-            await update.answer("You didn't set any custom thumbnail!", show_alert=True)
+            await query.answer("You didn't set any custom thumbnail!", show_alert=True)
         else:
-            await update.answer()
-            await bot.send_photo(
-                update.message.chat.id, 
+            await query.answer()
+            await client.send_photo(
+                query.message.chat.id, 
                 thumbnail, "Custom Thumbnail",
                 reply_markup=types.InlineKeyboardMarkup(
                     [
@@ -57,32 +57,32 @@ async def button(bot, update):
                     ]
                 )
             )
-    elif update.data == "deleteThumbnail":
-        await db.set_thumbnail(update.from_user.id, None)
-        await update.answer("Okay, I deleted your custom thumbnail. Now I will apply default thumbnail.", show_alert=True)
-        await update.message.delete(True)
-    elif update.data == "setThumbnail":
-        await update.message.edit_text(
+    elif query.data == "deleteThumbnail":
+        await db.set_thumbnail(query.from_user.id, None)
+        await query.answer("Okay, I deleted your custom thumbnail. Now I will apply default thumbnail.", show_alert=True)
+        await query.message.delete(True)
+    elif query.data == "setThumbnail":
+        await query.message.edit_text(
             text=Translation.TEXT,
             reply_markup=Translation.BUTTONS,
             disable_web_page_preview=True
         )
 
-    elif update.data == "triggerUploadMode":
-        await update.answer()
-        upload_as_doc = await db.get_upload_as_doc(update.from_user.id)
+    elif query.data == "triggerUploadMode":
+        await query.answer()
+        upload_as_doc = await db.get_upload_as_doc(query.from_user.id)
         if upload_as_doc:
-            await db.set_upload_as_doc(update.from_user.id, False)
+            await db.set_upload_as_doc(query.from_user.id, False)
         else:
-            await db.set_upload_as_doc(update.from_user.id, True)
-        await OpenSettings(update.message)
-    elif "close" in update.data:
-        await update.message.delete(True)
+            await db.set_upload_as_doc(query.from_user.id, True)
+        await OpenSettings(query.message)
+    elif "close" in query.data:
+        await query.message.delete(True)
 
-    elif "|" in update.data:
-        await youtube_dl_call_back(bot, update)
-    elif "=" in update.data:
-        await ddl_call_back(bot, update)
+    elif "|" in query.data:
+        await youtube_dl_call_back(client, query)
+    elif "=" in query.data:
+        await ddl_call_back(client, query)
 
     else:
-        await update.message.delete()
+        await query.message.delete()
