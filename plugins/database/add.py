08dd581114698f818e747b543bc10ff840d70fd8
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-10 23:52:21 +0530

    Update add.py

--- plugins/database/add.py
+++ plugins/database/add.py
@@ -1,14 +1,12 @@
-# (c) @AbirHasan2005
-
 from plugins.config import Config
 from .database import db
 from pyrogram import Client
 from pyrogram.types import Message
 
 
-async def add_user_to_database(bot: Client, cmd: Message):
-    if not await db.is_user_exist(cmd.from_user.id):
-        await db.add_user(cmd.from_user.id)
+async def add_user_to_database(bot: Client, message: Message):
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id)
         
 
 
